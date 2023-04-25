d3174a9adfb942381921932455e77ad5661aa445
Miguel Corredera Payo <co.miguel12@gmail.com>
2023-04-25 21:04:59 +0200

    create averages

--- script/05_spotify_scrape.R
+++ script/05_spotify_scrape.R
@@ -12,8 +12,8 @@ install.packages("ggjoy")
 install.packages("spotifyr")
 
 #set system values - insert information provided by spotify for developers
-Sys.setenv(SPOTIFY_CLIENT_ID = "")
-Sys.setenv(SPOTIFY_CLIENT_SECRET = "")
+Sys.setenv(SPOTIFY_CLIENT_ID = "cd7d988f8b034148bdcd921c804f6117")
+Sys.setenv(SPOTIFY_CLIENT_SECRET = "2ebdda53a31a4ef5828b31e947e179df")
 
 access_token <- get_spotify_access_token()
 
@@ -65,3 +65,54 @@ bad_bunny %>%
 
 #I save the graph
 ggsave("output/plots/danceability_albums_bunny.png", width = 16, height = 9)
+
+#Get different from top 50 playlists in the last 11 years (2022 not available)
+top100_2011 <- get_playlist_tracks("37i9dQZF1DXcagnSNtrGuJ")
+top100_2012 <- get_playlist_tracks("37i9dQZF1DX0yEZaMOXna3")
+top100_2013 <- get_playlist_tracks("37i9dQZF1DX3Sp0P28SIer")
+top100_2014 <- get_playlist_tracks("37i9dQZF1DX0h0QnLkMBl4")
+top100_2015 <- get_playlist_tracks("37i9dQZF1DX9ukdrXQLJGZ")
+top100_2016 <- get_playlist_tracks("37i9dQZF1DX8XZ6AUo9R4R")
+top100_2017 <- get_playlist_tracks("37i9dQZF1DWTE7dVUebpUW")
+top100_2018 <- get_playlist_tracks("37i9dQZF1DXe2bobNYDtW8")
+top100_2019 <- get_playlist_tracks("37i9dQZF1DWVRSukIED0e9")
+top100_2020 <- get_playlist_tracks("2fmTTbBkXi8pewbUvG3CeZ")
+top100_2021 <- get_playlist_tracks("5GhQiRkGuqzpWZSE7OU4Se")
+
+#I create one data frame with all the averages over the last 11 years 
+average_min <- data.frame(mean(top100_2011$track.duration_ms)) %>% 
+  mutate(mean(top100_2012$track.duration_ms)) %>% 
+  mutate(mean(top100_2013$track.duration_ms)) %>% 
+  mutate(mean(top100_2014$track.duration_ms)) %>% 
+  mutate(mean(top100_2015$track.duration_ms)) %>% 
+  mutate(mean(top100_2016$track.duration_ms)) %>% 
+  mutate(mean(top100_2017$track.duration_ms)) %>% 
+  mutate(mean(top100_2018$track.duration_ms)) %>% 
+  mutate(mean(top100_2019$track.duration_ms)) %>% 
+  mutate(mean(top100_2020$track.duration_ms)) %>% 
+  mutate(mean(top100_2021$track.duration_ms))
+
+#I turn the columns into rows
+average_min_all <- as.data.frame(t(average_min))
+
+#rename rows
+row.names(average_min_all)[row.names(average_min_all) == "mean.top100_2011.track.duration_ms."] <- "2011"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2012$track.duration_ms)"] <- "2012"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2013$track.duration_ms)"] <- "2013"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2014$track.duration_ms)"] <- "2014"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2015$track.duration_ms)"] <- "2015"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2016$track.duration_ms)"] <- "2016"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2017$track.duration_ms)"] <- "2017"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2018$track.duration_ms)"] <- "2018"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2019$track.duration_ms)"] <- "2019"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2020$track.duration_ms)"] <- "2020"
+row.names(average_min_all)[row.names(average_min_all) == "mean(top100_2021$track.duration_ms)"] <- "2021"
+
+#rename columns
+colnames(average_min_all)[colnames(average_min_all) == 'V1'] <- 'average_time'
+
+#I divide now every song by 60'000 to receive the average in Minutes. The average is now in milliseconds.
+
+average_min_all$average_time <- average_min_all$average_time / 60000
+
+
